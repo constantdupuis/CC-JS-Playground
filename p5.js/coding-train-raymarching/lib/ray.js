7f2dc8cdad6c3cf7366c784bd03126a04845b43e
Constant Dupuis <cdu@pulsar.be>
2024-06-21 13:19:24 +0200

    Advance in ray marching :-)

--- p5.js/coding-train-raymarching/lib/ray.js
+++ p5.js/coding-train-raymarching/lib/ray.js
@@ -5,13 +5,58 @@ class Ray {
         this.angle = angle;
     }
 
-    show(len)
+    march(objects)
     {
-        push();
-        translate(this.pos.x, this.pos.y);
-        const v  = p5.Vector.fromAngle(this.angle);
-        //v.setMag(len);
-        line(0, 0, v.x * len, v.y * len);
-        pop();
+        let current = this.pos.copy();
+        let counter = 0;
+        let maxIteration = 3;
+        
+
+        while(counter < maxIteration)
+        {
+            let closestObject = null;
+            let shortestDistance = Infinity;
+
+            for(let s of objects)
+            {
+                let d = signedDistance(current, s.pos, s.r);
+                if( d < shortestDistance )
+                {
+                    shortestDistance = d;
+                    closestObject = s;
+                }
+            }
+
+            if( shortestDistance < 5){
+                break;
+            }
+
+            const v  = p5.Vector.fromAngle(this.angle, shortestDistance);
+
+            push();
+            stroke(255,counter*50 ,200);
+            noFill();
+            translate(current.x, current.y);
+            ellipse(0, 0, shortestDistance * 2);
+            //line(0, 0, v.x, v.y);
+            pop();
+
+            current.add(v);
+
+            counter++;
+        }
+        //this.show(record);
     }
+
+    // show(len)
+    // {
+    //     push();
+    //     stroke(255,0 ,200);
+    //     noFill();
+    //     translate(this.pos.x, this.pos.y);
+    //     const v  = p5.Vector.fromAngle(this.angle, len);
+    //     ellipse(0, 0, len * 2);
+    //     line(0, 0, v.x, v.y);
+    //     pop();
+    // }
 }
--- p5.js/coding-train-raymarching/sketch.js
+++ p5.js/coding-train-raymarching/sketch.js
@@ -1,31 +1,39 @@
 
-let stuff = [];
+let objects = [];
 let ray;
 function setup() {
   createCanvas(800, 800);
-  stuff.push(new Boundary(300,200,50));
-  ray = new Ray(100,200,0);
+  for( let i = 0; i < 3;i++)
+  {
+    let r = random(10,50); 
+    let x = random(r, width - r);
+    let y = random(r, height - r);
+    objects.push(new Boundary(x, y, r));
+  }
+  
+  ray = new Ray(width/2, height/2, 0);
 }
 
 function draw() 
 {
   background(0);
-  ray.show();
-  for( let s of stuff)
+  //ray.show();
+  for( let s of objects)
   {
     s.show();
   }
 
-  const d = signedDistance( ray, stuff[0]);
-  ray.show(d);
+  ray.march(objects);
+
+  // const d = signedDistance( ray, stuff[0]);
+  // ray.show(d);
 }
 
-function signedDistance(ray, blob)
+function signedDistance(a, b, r)
 {
-  const d = p5.Vector.dist(ray.pos, blob.pos);
-  return d - blob.r;
+  const d = p5.Vector.dist(a, b);
+  return d - r;
 }
-
 
 function keyPressed() {
   if ((key == 'S') || (key == 's')) {
@@ -33,11 +41,9 @@ function keyPressed() {
   }
 }
 
-function mousePressed(event) {
-}
+function mousePressed(event) {}
 
-function mouseDragged(event) {
-}
+function mouseDragged(event) {}
 
 
 // function mouseReleased(event) {
